440faf7cc798baabded2146f656df63595365e12
JayZhang <jayzhang1997@gmail.com>
2019-08-09 14:15:44 -0500

    Setup landing page UI.

---
+++ Bunny Eats/Controller/LandingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Bunny Eats
+//
+//  Created by Zhi Wei Zhang on 8/7/19.
+//  Copyright © 2019 Zhi Wei Zhang. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    @IBOutlet weak var blurredView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = blurredView.bounds
+        blurredView.addSubview(blurView)
+        
+        blurredView.alpha = 0.85
+        blurredView.layer.cornerRadius = 10
+        blurredView.layer.masksToBounds = true
+    }
+    
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        
+    }
+}
