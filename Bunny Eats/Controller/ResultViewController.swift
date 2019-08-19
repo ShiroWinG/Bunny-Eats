b9b7270bb240fb04fb1188d3ea4c64fd37a7628a
JayZhang <jayzhang1997@gmail.com>
2019-08-19 01:36:08 -0400

    Implemented Yelp API, successfully able to retrieve JSON result based on search term.

--- Bunny Eats/Controller/ResultViewController.swift
+++ Bunny Eats/Controller/ResultViewController.swift
@@ -7,21 +7,27 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-class ResultViewController: UIViewController {
+class ResultViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var blurredView: UIView!
     @IBOutlet weak var bgImage: UIImageView!
     @IBOutlet weak var food: UILabel!
     @IBOutlet weak var foodDescription: UILabel!
     
+    let YELP_URL = "https://api.yelp.com/v3/businesses/search"
+    let API_KEY = "zg-HdaMM_6ULbi8bL_xSBJLrFPUI7FxgMQpPIL25MS3niImiOYxPwBh-VPvQK2MvYlSZUVsnf1HqCrsQ86C8vblUKjZ2LrI7f0CJ0ISjkUZj4-3Y9v9u21jFvSxaXXYx"
+    let locationManager = CLLocationManager()
     var weatherResult: String = ""
     var foodDict = [String: String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Setup blurred view and landing label
+        //Setup blurred view and landing labels
         let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
         let blurView = UIVisualEffectView(effect: blurEffect)
         blurView.frame = blurredView.bounds
@@ -37,8 +43,15 @@ class ResultViewController: UIViewController {
         print(weatherResult)
         weatherResult = weatherResult.filter { !$0.isNewline && !$0.isWhitespace }
         adaptiveResult(weather: weatherResult)
+        
+        //Set up the location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
+    //Food result and its descriptions
     func adaptiveResult(weather: String){
         bgImage.image = UIImage(named: weather)
         
@@ -55,10 +68,66 @@ class ResultViewController: UIViewController {
             print("Error D:")
         }
         
-        
         let componentArray = Array(foodDict.keys)
         food.text = componentArray[0]
         foodDescription.text = foodDict[componentArray[0]]
+    }
+    
+    //Location related methods
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            
+            let latitude = String(location.coordinate.latitude)
+            let longtitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["term": food.text!, "latitude": latitude, "longitude": longtitude]
+            
+            let header : [String: String] = ["Authorization": "Bearer \(API_KEY)"]
+            
+            getFoodData(url: YELP_URL, parameters: params, AuthHeader: header)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        //holderLabel.text = "Location Unavailable :["
+    }
+    
+    //Yelp query
+    func getFoodData(url: String, parameters: [String: String], AuthHeader: [String: String]) {
+        Alamofire.request(url, method: .get, parameters: parameters, headers: AuthHeader).responseJSON {
+            response in
+            if response.result.isSuccess {
+                
+                let foodJSON : JSON = JSON(response.result.value!)
+                self.updateFoodData(json: foodJSON)
+                
+                print(foodJSON)
+                
+            }
+            else {
+                //self.holderLabel.text = "Connection Issues"
+            }
+        }
+    }
+    
+    func updateFoodData(json: JSON) {
         
+        //Requirement: resturant name, review, address
+        /*
+        if let tempResult = json["main"]["temp"].double {
+            
+            temp = (9/5) * (tempResult - 273) + 32
+            city = json["name"].stringValue
+            
+            //updateUIWithFoodData()
+        }
+        else {
+            //holderLabel.text = "Results Unavailable D:"
+        }
+        */
     }
 }
